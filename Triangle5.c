b69e55046e5f8cd3c5e197a2830f9139db584e76
Cobbmann1 <Supawit_Thiti@hotmail.com>
2018-01-11 19:15:04 +0700

    AddTriangle5

---
+++ Triangle5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int line, n1, n2, n3,space;
+	printf("Enter Line : ");
+	scanf("%d", &line);
+	space = line - 1;
+
+	for(n1 = 1; n1 <= line; n1++){
+		for(n2 = space; n2>0; n2--){
+			printf(" ");
+			}
+		for(n3 = 0; n3 < (2 * n1) -1 ; n3++){
+			printf("*");
+			}
+		printf("\n");
+		space--;
+		}
+		
+}
